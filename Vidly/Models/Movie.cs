adb3485e496473b63381a9ac00f73b87475a3866
Gohar.Rehman <gohar.rehman@tpsonline.com>
2022-03-05 20:17:43 +0500

    Section 4: Excercise Add/Edit Movie

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -12,10 +12,14 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        [Required]
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
         public Genre Genre { get; set; }
         public DateTime DbDateTime { get; set; }
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+        [Display(Name = "Number in Stock")]
         public byte NumberInStock { get; set; }
     }
 }
