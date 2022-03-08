11c52edaa60784ab81e85256e25923af9866617a
Gohar.Rehman <gohar.rehman@tpsonline.com>
2022-03-09 00:01:23 +0500

    Apply validation summary

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -37,6 +37,7 @@ namespace Vidly.Controllers
 
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
